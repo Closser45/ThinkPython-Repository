3451904e6edc88ae18abb8dd2040010d20b67f81
Leigh Closser <leighclosser@Leighs-MacBook-Air.local>
2018-09-02 12:44:28 -0400

    Added to Exercise 4.3

---
+++ 4.3.py
@@ -0,0 +1,11 @@
+def pie(t, sides, radius):
+	import math
+	for i in range(sides):
+		t.fd(radius)
+		t.bk(radius)
+		t.lt(360 / sides)
+	t.fd(radius)
+	t.lt(180 - (180 - (360 / sides)) / 2)
+	for i in range(sides):
+		t.fd(math.sin(360 / sides / 2 / 180 * math.pi) * radius * 2)
+		t.lt(360 / sides)
