3530c6044f496f20be83ee0ba6acb73ccef6a388
Leigh Closser <leighclosser@Leighs-MacBook-Air.local>
2018-09-02 13:10:03 -0400

    Fixed

--- 4.3.py
+++ 4.3.py
@@ -1,5 +1,5 @@
+import math
 def pie(t, sides, radius):
-	import math
 	for i in range(sides):
 		t.fd(radius)
 		t.bk(radius)
@@ -10,4 +10,3 @@ def pie(t, sides, radius):
 		t.fd(math.sin(360 / sides / 2 / 180 * math.pi) * radius * 2)
 		t.lt(360 / sides)
 		#good work me
-		
